084fef67ff383647c92b7db35c221e8548a4be75
Terra Brown <superloach42@gmail.com>
2021-03-24 13:08:51 -0400

    remove FindPrefix, just have LinePrefix now

--- prefix.go
+++ prefix.go
@@ -12,13 +12,27 @@ type Prefix struct {
 	Clean string
 }
 
-// FindPrefix finds the first prefix that satisfies fn.
-func (r *Route) FindPrefix(
+// LinePrefix finds the first suitable prefix that matches the given line.
+func (r *Route) LinePrefix(
 	g discord.GuildID,
-	mme *discord.Member,
 	me discord.User,
-	fn func(Prefix) bool,
+	mme *discord.Member,
+	line string,
 ) (Prefix, bool) {
+	line = strings.TrimSpace(line)
+
+	used := func(pfx Prefix) bool {
+		if strings.HasPrefix(line, pfx.Value) {
+			return true
+		}
+
+		if strings.HasPrefix(line, pfx.Clean) {
+			return true
+		}
+
+		return false
+	}
+
 	// guild prefix
 	pfxv, ok := r.GetPrefix(g)
 	if !ok {
@@ -30,22 +44,22 @@ func (r *Route) FindPrefix(
 		Value: pfxv,
 		Clean: pfxv,
 	}
-	if ok && fn(pfx) {
+	if ok && used(pfx) {
 		return pfx, true
 	}
 
 	// member prefix
 	if mme != nil {
 		pfx = Prefix{
-			Value: mme.Mention(),
-			Clean: me.Username,
+			Value: mme.Mention() + " ",
+			Clean: "@" + me.Username + " ",
 		}
 
 		if mme.Nick != "" {
-			pfx.Clean = "@" + mme.Nick
+			pfx.Clean = "@" + mme.Nick + " "
 		}
 
-		if fn(pfx) {
+		if used(pfx) {
 			return pfx, true
 		}
 	}
@@ -55,23 +69,9 @@ func (r *Route) FindPrefix(
 		Value: me.Mention(),
 		Clean: "@" + me.Username,
 	}
-	if fn(pfx) {
+	if used(pfx) {
 		return pfx, true
 	}
 
 	return Prefix{}, false
-}
-
-// LinePrefix finds the first suitable prefix that matches the given line.
-func (r *Route) LinePrefix(
-	g discord.GuildID,
-	me discord.User,
-	mme *discord.Member,
-	line string,
-) (Prefix, bool) {
-	line = strings.TrimSpace(line)
-
-	return r.FindPrefix(g, mme, me, func(pfx Prefix) bool {
-		return strings.HasPrefix(line, pfx.Value)
-	})
 }
